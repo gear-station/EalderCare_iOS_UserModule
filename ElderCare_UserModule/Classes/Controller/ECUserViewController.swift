356ab0ac5245933f292ebfe47a9de3c32c0a8d83
Jia Jia <jiafujia123@gmail.com>
2019-05-30 12:39:21 +0900

    First commit

---
+++ ElderCare_UserModule/Classes/Controller/ECUserViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ECUserViewController.swift
+//  ElderCare_UserModule
+//
+//  Created by 贾富佳 on 2019/5/30.
+//
+
+import UIKit
+
+class ECUserViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        setUI()
+    }
+    
+    private func setUI() {
+        view.backgroundColor = UIColor.cyan
+    }
+    
+}
